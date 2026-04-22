08d013b15a6a94a7ec2cc84e8d7f52b471ae9390
MrMaksiStudios <ozcanemreoner@gmail.com>
2026-04-22 21:40:03 +0300

    Error Management
    Fixed errors in mission system

--- Assets/Scripts/MISSIONS/GameEvents.cs
+++ Assets/Scripts/MISSIONS/GameEvents.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public static class GameEvents
 {
     private static Dictionary<string, Action> eventDictionary = new();
+    private static HashSet<string> currentlyTriggering = new();
 
     public static void Subscribe(string eventName, Action listener)
     {
@@ -21,7 +22,17 @@ public static class GameEvents
 
     public static void Trigger(string eventName)
     {
-        if (eventDictionary.ContainsKey(eventName))
-            eventDictionary[eventName]?.Invoke();
+        if (currentlyTriggering.Contains(eventName)) return;
+
+        currentlyTriggering.Add(eventName);
+        try
+        {
+            if (eventDictionary.ContainsKey(eventName))
+                eventDictionary[eventName]?.Invoke();
+        }
+        finally
+        {
+            currentlyTriggering.Remove(eventName);
+        }
     }
 }
